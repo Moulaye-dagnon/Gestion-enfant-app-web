dd30075916015683e216edb5ff870da73b72a399
ibrahima Coulibaly <boulby9023@gmail.com>
2026-05-26 09:47:31 +0000

    modification du fichier ConnexionDB

--- src/main/java/com/denkolochi/configuration/ConnexionDB.java
+++ src/main/java/com/denkolochi/configuration/ConnexionDB.java
@@ -8,20 +8,64 @@ public class ConnexionDB {
 	
 	
 
-	    public static Connection getConexion() {
-	        try {
-	            Connection conexion = DriverManager.getConnection(DbConstConfig.url  ,DbConstConfig.utilisateur ,DbConstConfig.mot_de_passe);
-	            System.out.println("Connecté à la base de données !");
-	            return conexion;
-	        } catch (Exception e) {
-	            System.out.println("Erreur de connexion : " + e.getMessage());
-	            return null;
-	        }
-	    }
-	    
+	private static String  url = "jdbc:mysql://localhost:3306/suivi_enfant?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8";
+    private static String utilisateur = "root";
+    private static String mot_de_passe = "6240";
+
+    private static  ConnexionDB instance = null;
+    private  Connection connection= null;
+
+    public ConnexionDB() {
+        try {
+            this.connection = DriverManager.getConnection(url, utilisateur, mot_de_passe);
+            System.out.println("Connexion MySQL établie avec succès.");
+        } catch(SQLException e){
+            System.err.println("Erreur de connexion à la base de données.");
+            System.err.println("Code SQL  : " + e.getErrorCode());
+            System.err.println("Message   : " + e.getMessage());
+        }
+    }
+
+    public static ConnexionDB getInstance() {
+       try {
+           if (instance == null ){
+                instance = new  ConnexionDB();
+            }
+       } catch (Exception e) {
+          System.err.println("probleme de connection : " + e.getMessage());
+       }
+
+       return  instance;
+    }
+
+
+    public  Connection getconnection() {
+       try {
+           if (instance == null | connection.isClosed()){
+                System.out.println("Reconnexion en cours...");
+                instance = new  ConnexionDB();
+            }
+       } catch (SQLException e) {
+         System.err.println("Erreur lors de la vérification de la connexion : " + e.getMessage());
+       }
+
+       return  connection;
+    }
+
+    public void fermer() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                instance = null;
+                System.out.println("Connexion fermée.");
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur lors de la fermeture : " + e.getMessage());
+        }
+    }
 	    public static void main(String[] args) {
 	        try {
-	            Connection con = ConnexionDB.getConexion();
+	            Connection con = ConnexionDB.getInstance().getconnection();
 	            if (con != null) {
 	                Statement pont = con.createStatement();
 	                String sql = "INSERT INTO capacites VALUES (NULL, 'Emotionnelle')";
